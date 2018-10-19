3af8dedbc92981b0d2902d2f0fbf96330b24602d
ZacharyJia <jia199474@gmail.com>
2018-10-19 17:46:21 +0800

    删除自动sckey生成

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -69,13 +68,6 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
-    }
-
-    protected function registered(Request $request, $user)
-    {
-        $sckey = str_random(32);
-        $user['sckey'] = $sckey;
-        $user->save();
     }
 
 }
